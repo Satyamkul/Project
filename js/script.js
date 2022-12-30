a10d6db4d767a9b64cf17445b476cda666eec1c5
Satyamkul <satyamkul2002@gmail.com>
2022-12-30 14:34:01 +0530

    .

--- js/script.js
+++ js/script.js
@@ -37,35 +37,3 @@ $(document).ready(function () {
 });
 
 // slider js
-var currentSlide = 0;
-
-const slides = document.querySelectorAll(".slide");
-const init = (n) => {
-  slides.forEach((slide) => {
-    slide.style.display = "none";
-
-    
-  });
-  slides[n].style.display = "block";
-};
-
-document.addEventListener("DOMContentLoaded", init(currentSlide));
-const next = () => {
-  currentSlide >= slides.length - 1 ? (currentSlide = 0) : currentSlide++;
-
-  init(currentSlide);
-};
-
-const prev = () => {
-  currentSlide <= 0 ? (currentSlide = slides.length - 1) : currentSlide--;
-
-  init(currentSlide);
-};
-
-document.querySelector(".next").addEventListener("click", next);
-
-document.querySelector(".prev").addEventListener("click", prev);
-setInterval(() => {
-  next();
-}, 5000);
-
